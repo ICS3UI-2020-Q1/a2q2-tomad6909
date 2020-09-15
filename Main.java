7ade23c4b48ee3ca26ea658fa29472b083ee0125
tomad6909 <tomad6909@wrdsb.ca>
2020-09-15 15:17:06 +0000

    Finished A2Q2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program outputs the biggest integer registered
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for the user
+    Scanner input = new Scanner(System.in);
+
+    // ask user for integer
+    System.out.println("Please enter an integer:");
+
+    int integer1 = input.nextInt();
+
+    // ask user for another integer
+    System.out.println("Please enter another integer:");
+
+    int integer2 = input.nextInt();
+
+    // determine which integer is bigger
+    if(integer1 > integer2){
+      System.out.println("The biggest number is " + integer1 + ".");
+    } else if(integer1 == integer2){
+      System.out.println("There is no biggest number.");
+    } else {
+      System.out.println("The biggest number is " + integer2 + ".");
+    }
   }
 }
+
